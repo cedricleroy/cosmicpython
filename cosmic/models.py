d18572d888601c6d8aff588bfd71a2438a623462
Cedric Leroy <cedie73@hotmail.fr>
2020-06-02 22:05:05 -0500

    Domain model notes and comments

--- cosmic/models.py
+++ cosmic/models.py
@@ -1,3 +1,4 @@
+""" Domain modeling exercise (aka Business Logic) """
 from typing import Set
 
 from pydantic import BaseModel, Field
@@ -12,6 +13,9 @@ class OrderLine(BaseModel):
 
     # NOTE: Using a dataclass here to get a hash automatically
     # https://docs.python.org/3/library/dataclasses.html#dataclasses.dataclass
+    # We could also reimplement __eq__ and __hash__ ourselves.
+
+    # NOTE: This is a `value` object, identified by the data it holds.
 
 
 class BatchError(Exception):
@@ -24,7 +28,7 @@ class Batch(BaseModel):
     quantity: int = Field(int, ge=0)
     order_lines: Set[OrderLine] = set()
 
-    def allocate(self, order_line: OrderLine):
+    def allocate(self, order_line: OrderLine) -> None:
         if order_line in self.order_lines:
             return
         if order_line.sku != self.sku:
@@ -37,10 +41,14 @@ class Batch(BaseModel):
         self.quantity -= order_line.quantity
         self.order_lines.add(order_line)
 
-    def deallocate(self, order_line: OrderLine):
+    def deallocate(self, order_line: OrderLine) -> None:
         if order_line not in self.order_lines:
             msg = 'Order line {} has is not allocated'.format(order_line)
             raise BatchError(msg)
         self.quantity += order_line.quantity
         self.order_lines.remove(order_line)
+
+    # NOTE: This is an `entity` object - long-lived identity where some
+    # of the data it holds can change. Comparison and hashability have
+    # to be implemented with __eq__ and __hash__.
 
