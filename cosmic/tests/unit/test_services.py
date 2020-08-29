2152eb26dc5a542f351fc5911bc786adb5b77a02
Cedric Leroy <cedie73@hotmail.fr>
2020-08-29 11:22:02 -0500

    Add service tests

--- cosmic/tests/unit/test_services.py
+++ cosmic/tests/unit/test_services.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import pytest
+
 from cosmic import models, services
 
 
@@ -24,9 +26,28 @@ class FakeRepository:
         return list(self._batches)
 
 
+def test_commits():
+    line = models.OrderLine(sku="OMINOUS-MIRROR", quantity=10, orderid=1)
+    batch = models.Batch(reference="b1", sku="OMINOUS-MIRROR", quantity=100, eta=None)
+    repo = FakeRepository([batch])
+    session = FakeSession()
+
+    services.allocate(line, repo, session)
+    assert session.committed is True
+
+
 def test_returns_allocation():
     line = models.OrderLine(sku="COMPLICATED-LAMP", quantity=10, orderid=1)
     batch = models.Batch(reference="b1", sku="COMPLICATED-LAMP", quantity=100, eta=None)
     repo = FakeRepository([batch])
     result = services.allocate(line, repo, FakeSession())
     assert result == "b1"
+
+
+def test_error_for_invalid_sku():
+    line = models.OrderLine(sku="NONEXISTENTSKU", quantity=10, orderid=1)
+    batch = models.Batch(reference="b1", sku="AREALSKU", quantity=100, eta=None)
+    repo = FakeRepository([batch])
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate(line, repo, FakeSession())
